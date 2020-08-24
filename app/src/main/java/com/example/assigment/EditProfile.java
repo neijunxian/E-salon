7709b298af25f6f95df880a628b22a7950707066
neijunxian <neijx-wm19@student.tarc.edu.my>
2020-08-24 23:23:50 +0800

    commit by jun xian

--- app/src/main/java/com/example/assigment/EditProfile.java
+++ app/src/main/java/com/example/assigment/EditProfile.java
@@ -2,14 +2,18 @@ package com.example.assigment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
@@ -31,9 +35,14 @@ import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -51,17 +60,17 @@ import java.util.HashMap;
 public class EditProfile extends AppCompatActivity implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
 
     private EditText editTextName, editTextEmail, editTextPhone;
-    private TextView editTextBitrh;
+    private TextView editTextBitrh, btnSave;
     private ImageButton gallery, backBtn;
     private ImageView profile_image;
-    private Spinner spinner;
+    private RadioGroup radioGroup;
     private RadioButton buttonMale, buttonFemale;
     private static final int IMAGE_REQUEST = 1;
     private Uri imageUri;
     private StorageTask uploadTask;
-
+    public String passowrd;
     DatabaseReference reference;
-    FirebaseUser fuser;
+    private FirebaseUser fuser;
     StorageReference storageReference;
 
     @Override
@@ -70,6 +79,15 @@ public class EditProfile extends AppCompatActivity implements AdapterView.OnItem
         setContentView(R.layout.activity_edit_profile);
         gallery = findViewById(R.id.openGallery);
         editTextBitrh = findViewById(R.id.birth);
+        btnSave = findViewById(R.id.btnSave);
+        editTextName = findViewById(R.id.FullName);
+        editTextEmail = findViewById(R.id.Email);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextBitrh = findViewById(R.id.birth);
+        profile_image = findViewById(R.id.profile_image);
+        radioGroup = findViewById(R.id.radioGrp);
+        buttonMale = findViewById(R.id.radioButtonMale);
+        buttonFemale = findViewById(R.id.radioButtonFemale);
         storageReference = FirebaseStorage.getInstance().getReference("uploads");
         loadData();
 
@@ -86,6 +104,84 @@ public class EditProfile extends AppCompatActivity implements AdapterView.OnItem
                 showDatePickerDailog();
             }
         });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonSave_Click(v);
+            }
+        });
+    }
+
+    public void buttonSave_Click(View v) {
+        try {
+            String str_email = editTextEmail.getText().toString();
+            String str_fullname = editTextName.getText().toString();
+            String str_phone = editTextPhone.getText().toString();
+            String str_birth = editTextBitrh.getText().toString();
+            int radioId = radioGroup.getCheckedRadioButtonId();
+            TextView radio;
+            radio = findViewById(radioId);
+            String str_gender = radio.getText().toString();
+            if (TextUtils.isEmpty(str_fullname) || TextUtils.isEmpty(str_email)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle(R.string.error);
+                builder.setMessage(R.string.empty_fields);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                changeEmail(str_email, passowrd);
+                save(str_fullname, str_email, str_phone, str_gender, str_birth);
+            }
+        } catch (Exception e) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setTitle(R.string.error);
+            builder.setMessage(e.getMessage());
+            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.show();
+        }
+    }
+
+    public void changeEmail(final String email, String passowrd) {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(firebaseUser.getEmail(), passowrd);
+        firebaseUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    user.updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(EditProfile.this, "successful Updated Email", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void save(final String name, final String email, final String phone, final String gender, final String birth) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("fullname", name);
+        hashMap.put("email", email);
+        hashMap.put("phone", phone);
+        hashMap.put("gender", gender);
+        hashMap.put("birth", birth);
+        reference.updateChildren(hashMap);
+        Toast.makeText(EditProfile.this, "successful Updated", Toast.LENGTH_LONG).show();
     }
 
     private void showDatePickerDailog() {
@@ -101,7 +197,7 @@ public class EditProfile extends AppCompatActivity implements AdapterView.OnItem
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        String date = dayOfMonth + "/" + month + "/" + year ;
+        String date = dayOfMonth + "/" + month + "/" + year;
         editTextBitrh.setText(date);
     }
 
@@ -192,26 +288,18 @@ public class EditProfile extends AppCompatActivity implements AdapterView.OnItem
     }
 
     public void loadData() {
-        editTextName = findViewById(R.id.FullName);
-        editTextEmail = findViewById(R.id.Email);
-        editTextPhone = findViewById(R.id.editTextPhone);
-        editTextBitrh = findViewById(R.id.birth);
-        profile_image = findViewById(R.id.profile_image);
-        buttonMale = findViewById(R.id.radioButtonMale);
-        buttonFemale = findViewById(R.id.radioButtonFemale);
         fuser = FirebaseAuth.getInstance().getCurrentUser();
         final String[] editGender = new String[1];
         reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
-
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User user = snapshot.getValue(User.class);
-                editTextName.setText(user.getUsername());
+                editTextName.setText(user.getFullname());
                 editTextEmail.setText(user.getEmail());
                 editTextPhone.setText(user.getPhone());
                 editTextBitrh.setText(user.getBirth());
-                editTextBitrh.setText(user.getBirth());
+                passowrd = user.getPassword();
                 editGender[0] = (user.getGender());
                 if (editGender[0].equals("Male")) {
                     buttonMale.setChecked(true);
