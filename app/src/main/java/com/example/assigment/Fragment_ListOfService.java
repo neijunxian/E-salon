003afcfda940f90b255b6762a7bd2619a9e3c8c5
neijunxian <neijx-wm19@student.tarc.edu.my>
2020-09-09 15:43:55 +0800

    commit by jun xian

--- app/src/main/java/com/example/assigment/Fragment_ListOfService.java
+++ app/src/main/java/com/example/assigment/Fragment_ListOfService.java
@@ -1,18 +1,21 @@
 package com.example.assigment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
-public class Fragment_ListOfService extends Fragment {
+public class Fragment_ListOfService extends Fragment implements View.OnClickListener {
 
 
     public Fragment_ListOfService() {
@@ -25,6 +28,18 @@ public class Fragment_ListOfService extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_listofservice, container, false);
+        TextView Result = (TextView) view.findViewById(R.id.result);
+        Result.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.result:
+                Intent intent = new Intent(getActivity(),ServiceDetail.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
